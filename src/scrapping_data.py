4af539c929e6b4f2abd5737a59bb7ecd6b791c25
Beatriz Aguiar <beatrizaguiar.up@gmail.com>
2022-10-03 23:14:43 +0100

    web scrapping

--- src/scrapping_data.py
+++ src/scrapping_data.py
@@ -1,30 +1,42 @@
-from pydoc import synopsis
 import pandas as pd
 import numpy as np
 from bs4 import BeautifulSoup
 import urllib.request
 
 def get_synopsis(row):
-    print(row.tconst)
     fp = urllib.request.urlopen(f'https://www.imdb.com/title/{row.tconst}/plotsummary#synopsis')
     mybytes = fp.read()
     html_doc = mybytes.decode("utf8")
     fp.close()
 
     soup = BeautifulSoup(html_doc, 'html.parser')
-    print(soup.find(id='plot-synopsis-content'))
+    synopsis_ul = soup.find(id='plot-synopsis-content').li
+
+    is_empty = (synopsis_ul.p != None)
+
+
+    # if empty, skip or get summaries
+    if is_empty:
+        summaries_ul = soup.find(id='plot-summaries-content')
+        print(summaries_ul.li)
+        return summaries_ul.li[0]
+        return np.nan
+        
+    return synopsis_ul.get_text()
 
 
     
 
 def get_pages():
     df = pd.read_csv('./data/data.csv', delimiter=';')
-    #df['synopsis'] = df.head(1).apply(get_synopsis, axis=1)
-    v = df.head(1).apply(get_synopsis, axis=1)
+
+    df['synopsis'] = df.apply(get_synopsis, axis=1)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        print(df)
+    
 
 
 def main():
-    print("Hello!")
     get_pages()
 
 if __name__ == "__main__":
