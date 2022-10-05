26265bf70f34f04047d1ed56f5a3a1eea4a3dd2d
Beatriz Aguiar <beatrizaguiar.up@gmail.com>
2022-10-05 16:52:57 +0100

    organize

---
+++ Milestone1/src/create_dataset.py
@@ -0,0 +1,62 @@
+import pandas as pd
+
+title_basics = pd.read_csv('../downloads/title.basics.tsv', delimiter='\t', dtype={'isAdult': object})
+title_crew = pd.read_csv('../downloads/title.crew.tsv', delimiter='\t', dtype={'directors': str})
+title_ratings = pd.read_csv('../downloads/title.ratings.tsv', delimiter='\t')
+name_basics = pd.read_csv('../downloads/name.basics.tsv', delimiter='\t', dtype={'primaryName': str})
+
+
+# filter by titleType
+
+title_basics = title_basics[(title_basics['titleType'] == 'movie') | (title_basics['titleType'] == 'tvMovie')]
+title_basics.reset_index(inplace=True, drop=True)
+
+
+# remove movies before a certain year
+
+title_basics['startYear'] = pd.to_numeric(title_basics['startYear'], errors='coerce')
+title_basics = title_basics[title_basics['startYear'] > 1990]
+title_basics.reset_index(drop=True, inplace=True)
+
+
+# drop useless columns 
+
+title_basics.drop(labels=['endYear', 'titleType'], axis=1, inplace=True)
+title_crew.drop(labels=['writers'], axis=1, inplace=True)
+title_ratings.drop(labels=['numVotes'], axis=1, inplace=True)
+name_basics.drop(labels=['birthYear', 'deathYear', 'primaryProfession', 'knownForTitles'], axis=1, inplace=True)
+
+# create dataset
+
+df = title_basics.merge(title_crew, on='tconst', how='left').merge(title_ratings, on='tconst', how='left')
+
+
+# remove movies with ratings below 7 or null values
+
+df = df[(not df['averageRating'].isnull()) & (df['averageRating'] >= 7)]
+df.reset_index(inplace=True, drop=True)
+
+
+# explode movie rows with more than one director into multiple rows, each one with only one director code
+
+df = df.assign(directors=df['directors'].str.split(',')).explode('directors')
+df = df.merge(name_basics, left_on='directors', right_on='nconst', how='left')
+
+
+# drop useless columns
+
+df.drop(labels=['directors', 'nconst'], axis=1, inplace=True)
+df.rename(columns={'primaryName' : 'directors'}, inplace=True)
+
+
+# remove duplicate rows while joining director names
+
+df['directors'] = df['directors'].astype(str)
+directors_column = df.groupby(['tconst']).agg({'directors': ', '.join})['directors'].values
+
+df.drop_duplicates(subset='tconst', inplace=True)
+df['directors'] = directors_column
+
+# save dataset
+
+df.to_csv('../data/data.csv', index=False, sep=';')
