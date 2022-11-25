f82e3bf4768f66cf8f316012d9de9b2e26220327
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-11-25 11:47:50 +0000

    Milestone 3 setup

---
+++ milestone3/README.md
@@ -0,0 +1,9 @@
+# Milestone 3: Search System
+
+## Usage
+
+```sh
+docker build . -t imdb_movies
+
+docker run -p 8983:8983 --name imdb_movies imdb_movies
+```
---
+++ milestone3/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+precreate-core imdb_movies
+
+# Start Solr in background mode so we can use the API to upload the schema
+solr start
+
+sleep 10
+# Schema definition via API
+curl -X POST -H 'Content-type:application/json' \
+                --data-binary @/data/movie_schema.json \
+                http://localhost:8983/solr/imdb_movies/schema
+
+# Populate collection
+bin/post -c imdb_movies /data/data.json
+
+# Restart in foreground mode so we can access the interface
+solr restart -f
