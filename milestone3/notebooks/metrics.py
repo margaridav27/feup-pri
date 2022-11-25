fa2973a041307a9864a3c62f42f1429010734710
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-11-25 12:17:39 +0000

    Evaluation metrics

---
+++ milestone3/notebooks/metrics.py
@@ -0,0 +1,24 @@
+# Define custom decorator to automatically calculate metric based on key
+metrics = {}
+metric = lambda f: metrics.setdefault(f.__name__, f)
+
+@metric
+def ap(results, relevant):
+    """Average Precision"""
+    precision_values = [
+        len([
+            doc 
+            for doc in results[:idx]
+            if doc['tconst'] in relevant
+        ]) / idx 
+        for idx in range(1, len(results))
+    ]
+    return sum(precision_values)/len(precision_values)
+
+@metric
+def p10(results, relevant, n=10):
+    """Precision at N"""
+    return len([doc for doc in results[:n] if doc['tconst'] in relevant])/n
+
+def calculate_metric(key, results, relevant):
+    return metrics[key](results, relevant)
