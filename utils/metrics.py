62ab7a95e04f134924ca92aa5eb96b39617d21fa
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-12-02 13:02:22 +0000

    Avg@10 milestone3

--- utils/metrics.py
+++ utils/metrics.py
@@ -16,23 +16,36 @@ def ap(results, relevant):
     return sum(precision_values)/len(precision_values)
 
 @metric
+def ap10(results, relevant, n=10):
+    """Average Precision at 10"""
+    precision_values = [
+        len([
+            doc
+            for doc in results[:idx]
+            if doc['tconst'] in relevant
+        ]) / idx
+        for idx in range(1, n+1)
+    ]
+    return sum(precision_values)/len(precision_values)
+
+@metric
 def map(results, _):
     """Mean Average Precision"""
     return sum(results)/len(results)
 
 @metric
 def p10(results, relevant, n=10):
-    """Precision at N"""
+    """Precision at 10"""
     return len([doc for doc in results[:n] if doc['tconst'] in relevant])/n
 
 @metric
 def p15(results, relevant, n=15):
-    """Precision at N"""
+    """Precision at 15"""
     return len([doc for doc in results[:n] if doc['tconst'] in relevant])/n
 
 @metric
 def ia(results, relevant, n=10):
-    """Individual Assessment at N"""
+    """Individual Assessment at 10"""
     return "".join(["R" if doc['tconst'] in relevant else "N" for doc in results[:n]])
 
 def calculate_metric(key, results, relevant=None):
